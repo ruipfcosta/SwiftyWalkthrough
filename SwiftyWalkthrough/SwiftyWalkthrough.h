34e8a43937f5325f58990d528591108f4df25fd0
Rui Costa <rui.pfcosta@gmail.com>
2015-09-28 16:59:17 +0100

    Initial commit.

---
+++ SwiftyWalkthrough/SwiftyWalkthrough.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyWalkthrough.h
+//  SwiftyWalkthrough
+//
+//  Created by Rui Costa on 28/09/2015.
+//  Copyright © 2015 Rui Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyWalkthrough.
+FOUNDATION_EXPORT double SwiftyWalkthroughVersionNumber;
+
+//! Project version string for SwiftyWalkthrough.
+FOUNDATION_EXPORT const unsigned char SwiftyWalkthroughVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyWalkthrough/PublicHeader.h>
+
+
