ee5d59cb473be513f5925f04ca147591d28e71bf
Rui Costa <rui.pfcosta@gmail.com>
2015-09-28 17:18:08 +0100

    Created workspace and added example project.

---
+++ iOS Example/iOS Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Rui Costa on 28/09/2015.
+//  Copyright © 2015 Rui Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
