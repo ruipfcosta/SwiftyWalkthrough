3c3134c04d452ed77f4ab16030a872ea12d8ccaf
Jakob Mikkelsen <5514628+JMCPH@users.noreply.github.com>
2021-08-03 10:26:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,17 @@
+// swift-tools-version:5.4.0	
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyWalkthrough"
-)
-
+    name: "SwiftyWalkthrough",
+    platforms: [.iOS(.v9)],
+	products: [
+    	.library(
+	        name: "SwiftyWalkthrough",
+        	targets: ["SwiftyWalkthrough"])
+    	],
+	targets: [
+    	.target(
+	        name: "SwiftyWalkthrough",
+        	path: "Sources")
+       	]
+	)
